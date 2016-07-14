2f4f711603d3cc724e758c5f345d5e57b21d53b1
david <dpark543107@gmail.com>
2016-07-14 16:11:17 -0500

    debuggin heroku accounts

--- controllers/account.js
+++ controllers/account.js
@@ -12,13 +12,19 @@ controller.post('/login', function(req, res, next) {
 				 password: req.body.password
 				};
 	// http request with the login variable
+	console.log(login);
+	console.log('^-------------login')
 	var requestedLogin = accountRequest.login(login);
 
+	console.log(requestedLogin);
+	console.log('^----------------requestLogin')
 	// if successful, create sessions
 	if (requestedLogin.success === true) {
 		req.session.email = requestedLogin.searchHistory[0].email
 		req.session.account = requestedLogin;
-		console.log('----------------------req.session')
+		console.log('below----------------------req.session.account')
+		console.log(req.session.account)
+		console.log('below----------------------req.session.email')
 		console.log(req.session.email);
 		res.redirect('/weather');
 	} else {
@@ -32,13 +38,19 @@ controller.post('/signup', function(req, res, next) {
 				  password: req.body.password
 				 };
 	 // http request with signup variable
+	 console.log(signup);
+	console.log('^-------------signup')
 	var requestedSignup = accountRequest.signup(signup);
+	console.log(requestedSignup);
+	console.log('^----------------requestedSignup')
 	// if successful, create sessions
 	if (requestedSignup.success === true) {
 		req.session.email = requestedSignup.searchHistory[0].email
 		req.session.account = requestedSignup;
-		console.log('----------------------req.session')
-		console.log(req.session.account);
+		console.log('below----------------------req.session.account')
+		console.log(req.session.account)
+		console.log('below----------------------req.session.email')
+		console.log(req.session.email);
 		res.redirect('/weather');
 	} else {
 		res.redirect('/accounts');
