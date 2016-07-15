2abe11c2680821f0503e163b04b7d2eac30fc70c
david <dpark543107@gmail.com>
2016-07-14 22:25:05 -0500

    debugging weather input

--- controllers/weather.js
+++ controllers/weather.js
@@ -13,7 +13,14 @@ controller.post('/input', function(req, res, next) {
 	var inputLocation = req.body.area;
 	var inputAccount = req.session.email;
 
+	console.log(inputLocation)
+	console.log('^^^^^^^^^^^^^^^^^^inputlocation')
+	console.log(inputAccount)
+	console.log('^^^^^^^^^^^^^^^^^^inputAccount')
+
 	var requestedWeather = weatherRequest.weatherInput(inputLocation, inputAccount);
+	console.log(requestedWeather)
+	console.log('^^^^^^^^^^^^^^^^^^requestedWeather')
 	req.session.weather = requestedWeather;
 	res.redirect('/todayWeather');
 })
