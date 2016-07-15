5262b106494d242b3f4c3f957ef16a269bdaadb2
david <dpark543107@gmail.com>
2016-07-14 23:35:40 -0500

    changed convert time

--- public/javascripts/timestampConverter.js
+++ public/javascripts/timestampConverter.js
@@ -3,20 +3,32 @@ module.exports.convertTime = function(timestamp) {
 		yyyy = d.getFullYear(),
 		mm = ('0' + (d.getMonth() + 1)).slice(-2),	// Months are zero based. Add leading 0.
 		dd = ('0' + d.getDate()).slice(-2),			// Add leading 0.
-		hh = d.getHours(),
+		hh = d.getHours() - 5,
 		h = hh,
 		min = ('0' + d.getMinutes()).slice(-2),		// Add leading 0.
 		ampm = 'AM',
 		time;
 		
-	if (hh > 12) {
-		h = hh - 12;
+	// if (hh > 12) {
+	// 	h = hh - 12;
+	// 	ampm = 'PM';
+	// } else if (hh === 12) {
+	// 	h = 12;
+	// 	ampm = 'PM';
+	// } else if (hh == 0) {
+	// 	h = 12;
+	// }
+
+	if (hh > 7) {
+		h = hh - 7;
 		ampm = 'PM';
-	} else if (hh === 12) {
+	} else if (hh === 7) {
 		h = 12;
 		ampm = 'PM';
-	} else if (hh == 0) {
+	} else if (hh === -5) {
 		h = 12;
+	} else if (hh >= -4 && hh <= 5) {
+		h = hh + 7;
 	}
 	
 	time = h + ':' + min + ' ' + ampm;
